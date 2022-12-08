11554573faa038bf002a873ce9ae8b1e4aa7b3a7
Yusuf <93403926+YusufHoDYavuz@users.noreply.github.com>
2022-12-08 13:27:07 +0300

    added trail renderer

--- Assets/Scripts/BlockCoin.cs
+++ Assets/Scripts/BlockCoin.cs
@@ -8,12 +8,14 @@ public class BlockCoin : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        //float randomFallRot = Mathf.Clamp(0, -30, 30);
+
         if (other.transform.CompareTag("ChildCoin"))
         {
             Debug.Log(other.gameObject.name + " and " + transform.gameObject.name + " collided / -1 Coin");
 
             Destroy(other.gameObject.GetComponent<EarnCoin>());
-            other.gameObject.GetComponent<Collider>().isTrigger = false;
+            other.gameObject.GetComponent<BoxCollider>().isTrigger = false;
             other.gameObject.GetComponent<Rigidbody>().useGravity = true;
             CS.coinList.Remove(other.gameObject);
             other.gameObject.transform.SetParent(null);
