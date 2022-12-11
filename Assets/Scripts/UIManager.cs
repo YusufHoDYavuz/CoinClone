b5c931c45c86b8ac1cc213d483df7ee0f5f26e0c
Yusuf <93403926+YusufHoDYavuz@users.noreply.github.com>
2022-12-11 18:29:28 +0300

    aded swipe to play panel

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class UIManager : MonoBehaviour
 {
     public GameObject finishGameUI;
     public GameObject failGameUI;
+    public GameObject swipeToPlayText;
+    public GameObject swipeToPlayPanel;
+    public GameObject mainCoin;
+
+    private void Start()
+    {
+        swipeToPlayText.transform.DOScale(new Vector3(1.2f, 1.2f, 1.2f), 0.5f).SetLoops(-1,LoopType.Yoyo).SetEase(Ease.InOutSine);
+    }
 
     public void openFinishGameUI()
     {
@@ -22,6 +32,12 @@ public class UIManager : MonoBehaviour
     {
         finishGameUI.SetActive(false);
         failGameUI.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        swipeToPlayPanel.SetActive(false);
+        mainCoin.transform.GetComponent<MainCoin>().enabled = true;
     }
 
     IEnumerator failUI()
