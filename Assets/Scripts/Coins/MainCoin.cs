01aaf6d95eeae8b31b41799467becf8b27a6be13
Yusuf <93403926+YusufHoDYavuz@users.noreply.github.com>
2022-12-11 12:18:27 +0300

    added climbing stairs

--- Assets/Scripts/Coins/MainCoin.cs
+++ Assets/Scripts/Coins/MainCoin.cs
@@ -7,10 +7,13 @@ public class MainCoin : MonoBehaviour
 {
     [SerializeField] private float maxAngle;
     [SerializeField] private float coinsSpeed;
+    [SerializeField] private GameObject earnCoinPrefab;
+    [SerializeField] private GameObject mainCoinPrefab;
     [SerializeField] private CoinStack CS;
     [SerializeField] private BlockCoin BC;
 
     private bool isCollide;
+    private bool isCollideStairs;
     private List<GameObject> coinsList;
     private Rigidbody rb;
 
@@ -57,26 +60,31 @@ public class MainCoin : MonoBehaviour
             other.transform.GetComponent<Collider>().isTrigger = false;
             transform.position += new Vector3(0, 0.25f, 0);
 
-            if (coinsList.Count > 0)
+            if (isCollide == false && !isCollideStairs)
             {
-                rb.constraints = RigidbodyConstraints.None;
-                MainBlockCollider();
-                transform.DOMove(new Vector3(other.transform.position.x + 0.5f, other.transform.position.y + 0.2f, other.transform.position.z),1);
-                transform.DORotate(new Vector3(90, 0, 0), 1);
-
-
+                transform.gameObject.GetComponent<MeshRenderer>().enabled = false;
+                transform.GetChild(1).gameObject.SetActive(false);
+                GameObject newMainCoin = Instantiate(mainCoinPrefab, Vector3.zero, Quaternion.identity);
+                newMainCoin.transform.DOMove(new Vector3(other.transform.position.x + 0.5f, other.transform.position.y + 0.15f, other.transform.position.z), 1).SetEase(Ease.InBounce);
+                newMainCoin.transform.DORotate(new Vector3(90, 0, 0), 1);
+                newMainCoin.transform.GetComponent<MeshRenderer>().enabled = true;
+                newMainCoin.transform.GetComponent<MainCoin>().enabled = false;
+                newMainCoin.transform.GetComponent<CoinStack>().enabled = false;
+                newMainCoin.transform.GetComponent<PlayerController>().enabled = false;
+                newMainCoin.transform.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+                isCollideStairs = true;
             }
             else if(coinsList.Count > 0)
             {
-                /* for (int i = 0; i < coinsList.Count; i++)
-                {
-                    BC.ChildCoinCollide(coinsList[i].gameObject);
-                    coinsList[i].transform.DOMove(new Vector3(other.transform.position.x + 0.5f, other.transform.position.y + 0.2f, other.transform.position.z), 1).OnComplete(()=> { 
-                        coinsList[i].transform.DORotate(new Vector3(90, 0, 0), 1);
-                    });
-                    Debug.Log("Coin Last Index: ");
-                }
-                 */
+                coinsList[coinsList.Count - 1].gameObject.transform.GetComponent<MeshRenderer>().enabled = false;
+                BC.ChildCoinCollide(coinsList[coinsList.Count - 1].gameObject);
+                GameObject newEarnCoin = Instantiate(earnCoinPrefab, Vector3.zero, Quaternion.identity);
+                newEarnCoin.transform.DOMove(new Vector3(other.transform.position.x + 0.5f, other.transform.position.y + 0.15f, other.transform.position.z), 1).SetEase(Ease.InBounce);
+                newEarnCoin.transform.DORotate(new Vector3(90, 0, 0), 1);
+            }
+            else if (coinsList.Count == 0)
+            {
+                Time.timeScale = 0;
             }
         }
     }
